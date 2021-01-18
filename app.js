d304d91f5b157a0c1d59d61a6f153598816f6190
DillanR1 <dillanroby@yahoo.com>
2021-01-18 14:33:21 -0800

    form fields validated with regular expressions

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function validateName() {
 
 function validateZip() {
   const zip = document.getElementById("zip");
-  const re = /^[0-9]{5}(-[0-9]{4})$/;
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
 
   if (!re.test(zip.value)) {
     zip.classList.add("is-invalid");
@@ -25,6 +25,24 @@ function validateZip() {
   }
 }
 
-function validateEmail() {}
+function validateEmail() {
+  const email = document.getElementById("email");
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-function validatePhone() {}
+  if (!re.test(email.value)) {
+    email.classList.add("is-invalid");
+  } else {
+    email.classList.remove("is-invalid");
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById("phone");
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add("is-invalid");
+  } else {
+    phone.classList.remove("is-invalid");
+  }
+}
